ab2936d591f9a2f922e270a58d56be1cbdd486a9
x8091 <mr.honghainguyen@gmail.com>
2018-04-04 15:14:22 +0700

    Just to correct author

--- user.js
+++ user.js
@@ -1,1 +1,2 @@
+// This file is used for FF, so that it appears as Windows Client
 user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36");
