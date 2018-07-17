d73ad4ed7c35abdf1e7927dad58be20ca5bbddc5
Hai Nguyen <hai.nguyen@innogram.co>
2018-07-17 17:03:09 +0700

    Just a plain commit

--- user.js
+++ user.js
@@ -1,2 +1,3 @@
 // This file is used for FF, so that it appears as Windows Client
 user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36");
+// Why not another commit?
